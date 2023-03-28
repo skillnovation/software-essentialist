57d7bc959dc933d779b8097e357df8d8a17c8ce2
skillnovation <skillnovation.com@gmail.com>
2023-03-28 18:18:34 -0500

    [NEW]: fizzbuzz return string given num parameter

---
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -0,0 +1,3 @@
+export function fizzBuzz(num : Number){
+    return ""
+}
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,7 @@
+import {fizzBuzz} from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-
-
+    it("should return a string", () => {
+        expect(typeof fizzBuzz(5)).toBe("string");
+    })
 });
