bd02777d21ad74413113421b2145a9effbed9e4b
skillnovation <skillnovation.com@gmail.com>
2023-03-28 18:25:25 -0500

    [NEW] return fizz given 3

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,3 +1,3 @@
 export function fizzBuzz(num : Number){
-    return ""
+    return "3"
 }
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -4,4 +4,8 @@ describe("fizzbuzz", () => {
     it("should return a string", () => {
         expect(typeof fizzBuzz(5)).toBe("string");
     })
+
+    it("should return fizz for 3", () => {
+        expect(fizzBuzz(3)).toBe("3");
+    })
 });
