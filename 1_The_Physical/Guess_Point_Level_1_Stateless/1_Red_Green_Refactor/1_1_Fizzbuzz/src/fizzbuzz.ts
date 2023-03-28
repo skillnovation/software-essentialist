8603cd8ea54f630006a5a36dbac4d8fb13147ff5
skillnovation <skillnovation.com@gmail.com>
2023-03-28 18:28:36 -0500

    [NEW]: should return fizzbuzz given 15

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,4 +1,7 @@
 export function fizzBuzz(num : Number){
+    if(num === 15){
+        return "fizzbuzz"
+    }
     if(num === 3){
         return "fizz"
     }
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -12,4 +12,8 @@ describe("fizzbuzz", () => {
     it("should return fizz for 5", () => {
         expect(fizzBuzz(5)).toBe("buzz");
     })
+
+    it("should return fizzbuzz for 15", () => {
+        expect(fizzBuzz(15)).toBe("fizzbuzz");
+    })
 });
